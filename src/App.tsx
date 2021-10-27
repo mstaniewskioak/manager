97b62c68d76f6ae2425e40edfa8cee3cda8e5106
Dominik <dleszczynski89@gmail.com>
2021-10-27 10:38:15 +0200

    Added button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react"
 import TodoApp from "components/TodoApp"
+import Button from "components/Button"
 import "./App.css"
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <TodoApp />
+      
+      {/* <Button onClick={() => console.log("test")} name="Click"/> */}
+      
     </div>
   )
 }
---
+++ src/components/Button/Button.style.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+export const ButtonWrapper = styled.button`
+  width: auto;
+  padding: 8px 12px;
+  border-radius: 4px; 
+  background-color: #007bff;
+  color: #fff;
+  border: solid #007bff 2px;
+  outline: none;
+  cursor: pointer;
+  &:hover {
+    background-color: #fff;
+    color: #007bff;
+    border solid #007bff 2px;
+  }
+`
